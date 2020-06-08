b70af78a7e8ef77a768a83f62513131b542f2bce
karsten332 <karsten.sand@haugnett.no>
2020-06-08 20:51:54 +0200

    Input email

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+const CRED = require('./secret.js');
 (async () => {
-    const browser = await puppeteer.launch();
+    //console.log(CRED.password)
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    await page.goto('https://example.com');
+    await page.goto('https://facebook.com', { waitUntil: 'domcontentloaded' });
+
+    await page.waitForSelector("#email");
+
+    await page.type("#email", CRED.username)
+
     await page.screenshot({ path: 'example.png' });
 
     await browser.close();
